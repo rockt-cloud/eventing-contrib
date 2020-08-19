48a684a56cec21601f6e95e8259907e35dca0bc1
yiyiyimu <wosoyoung@gmail.com>
2020-08-19 17:08:47 +0800

    rocketmq channel

---
+++ vendor/github.com/cloudevents/sdk-go/protocol/rocketmq/v2/subscriber.go
@@ -0,0 +1,24 @@
+package rocketmq
+
+import (
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+)
+
+// The Subscriber interface allows us to configure how the subscription is created
+type Subscriber interface {
+	Subscribe(consumer rocketmq.PushConsumer, topic string, selector consumer.MessageSelector,
+		f func(context.Context, ...*primitive.MessageExt) (consumer.ConsumeResult, error)) error
+}
+
+// RegularSubscriber creates regular subscriptions
+type RegularSubscriber struct {
+}
+
+// Subscribe implements Subscriber.Subscribe
+func (s *RegularSubscriber) Subscribe(pc rocketmq.PushConsumer, topic string, selector consumer.MessageSelector,
+	f func(context.Context, ...*primitive.MessageExt) (consumer.ConsumeResult, error)) error {
+	return pc.Subscribe(topic, selector, f)
+}
+
+var _ Subscriber = (*RegularSubscriber)(nil)
